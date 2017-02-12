f86ff94419b1cacf3811e1745c4f618e91bae287
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-02-12 21:49:28 +0200

    create Hello World example

---
+++ src/main/java/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.tutorialspoint;
+
+/**
+ * @author Drusev
+ *
+ */
+public class HelloWorld {
+
+	private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+}
