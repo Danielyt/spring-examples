c732123be864ad615fc34295ee73a1b17433cfd1
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-02-13 21:14:28 +0200

    write unit test for singleton bean

---
+++ src/test/java/com/tutorialspoint/BeanScopeTest.java
@@ -0,0 +1,29 @@
+/** 
+ * 
+ */
+package com.tutorialspoint;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Drusev
+ *
+ */
+public class BeanScopeTest {
+	
+	private final ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	
+	@Test
+	public void testSingletonBean() {
+		HelloWorld bean1 = (HelloWorld) context.getBean("singletonHelloWorld");
+		assertNotNull(bean1);
+		HelloWorld bean2 = (HelloWorld) context.getBean("singletonHelloWorld");
+		assertNotNull(bean2);
+		assertSame(bean1, bean2);
+	}
+}
