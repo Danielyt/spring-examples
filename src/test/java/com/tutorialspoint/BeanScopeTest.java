661ea52e769b9260eec69dbaa3423219bb0849fd
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-02-13 21:18:23 +0200

    add prototype bean of type HelloWorld

--- src/test/java/com/tutorialspoint/BeanScopeTest.java
+++ src/test/java/com/tutorialspoint/BeanScopeTest.java
@@ -4,6 +4,7 @@
 package com.tutorialspoint;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
@@ -25,5 +26,14 @@ public class BeanScopeTest {
 		HelloWorld bean2 = (HelloWorld) context.getBean("singletonHelloWorld");
 		assertNotNull(bean2);
 		assertSame(bean1, bean2);
+	}
+	
+	@Test
+	public void testPrototypeBean() {
+		HelloWorld bean1 = (HelloWorld) context.getBean("prototypeHelloWorld");
+		assertNotNull(bean1);
+		HelloWorld bean2 = (HelloWorld) context.getBean("prototypeHelloWorld");
+		assertNotNull(bean2);
+		assertNotSame(bean1, bean2);
 	}
 }
