1761e4cacc7a0dee71de8d2691c18f67f465a187
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-02-15 21:40:50 +0200

    create MainApp to test lifecycle bean HelloWorld

---
+++ src/main/java/com/tutorialspoint/beanlifecycle/MainApp.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.tutorialspoint.beanlifecycle;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Drusev
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		try (AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml")) {
+			HelloWorld obj = (HelloWorld) context.getBean("lifeCycledHelloWorld");
+			obj.getMessage();
+			context.registerShutdownHook();
+		}
+	}
+}
