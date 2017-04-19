420a31bb39cfc431f28f415b3ddd49f23755557c
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-04-19 21:10:40 +0300

    add example of xml based aop

---
+++ src/main/java/com/tutorialspoint/aop/MainApp.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.tutorialspoint.aop;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Drusev
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("AOP_XML_based.xml")) {
+			Student student = (Student) context.getBean("student");
+			student.getName();
+			student.getAge();
+			student.printThrowException();
+		}
+	}
+}
