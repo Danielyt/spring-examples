834cca872701727dfe40f3234b65e53cb56b0c8f
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-04-22 17:58:54 +0300

    add example of declarative transaction management

---
+++ src/main/java/com/tutorialspoint/transaction/declarative/StudentMarksMapper.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.tutorialspoint.transaction.declarative;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+/**
+ * @author Drusev
+ *
+ */
+public class StudentMarksMapper implements RowMapper<StudentMarks> {
+
+	@Override
+	public StudentMarks mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+		StudentMarks studentMarks = new StudentMarks();
+		studentMarks.setId(rs.getInt("id"));
+		studentMarks.setName(rs.getString("name"));
+		studentMarks.setAge(rs.getInt("age"));
+		studentMarks.setSid(rs.getInt("sid"));
+		studentMarks.setMarks(rs.getInt("marks"));
+		studentMarks.setYear(rs.getInt("year"));
+		return studentMarks;
+	}
+}
