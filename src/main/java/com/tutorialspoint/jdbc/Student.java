9f2fdf7dc2ae1787d8de481f3efed936c57f3f84
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-04-22 14:06:38 +0300

    add example of jdbc

---
+++ src/main/java/com/tutorialspoint/jdbc/Student.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.tutorialspoint.jdbc;
+
+/**
+ * @author Drusev
+ *
+ */
+public class Student {
+	private Integer age;
+	private String name;
+	private Integer id;
+
+	public void setAge(final Integer age) {
+		this.age = age;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+}
